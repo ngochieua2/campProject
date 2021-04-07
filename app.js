230eb92b5f1131315878aca8f746d14f1a228208
Ngoc Hieu Nguyen <ngochieua2@gmail.com>
2021-04-08 03:24:49 +0930

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('HELLO FROM YELP CAMP!!')
+})
+
+app.listen(3000, ()=> {
+    console.log('Hi, server is on port 3000');
+})
