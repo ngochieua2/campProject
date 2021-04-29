7aaeac8e91b3e21b2f76fd827fea94ec0cbdf9fb
Ngoc Hieu Nguyen <ngochieua2@gmail.com>
2021-04-30 00:48:03 +0930

    add delete review and upgrate delete camp

--- app.js
+++ app.js
@@ -104,6 +104,14 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req,res) 
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync( async(req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: {reviews: reviewId}}); 
+    // pull will remove an existing array all insances of value or value that match a specified condition
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}))
+
 app.all('*', (req, res, next) => {
     next(new expressError('Page Not Found', 404));
 })
--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -14,5 +15,16 @@ const CampgroundSchema = new Schema({
         }
     ]
 });
+
+//need to check type of middleware like findbyIdAndDelete refer to findOneAndDelete
+//this query middleware is use to delete review after delete a campground
+// in camp.review just has ID of all review. use populate to show more
+CampgroundSchema.post('findOneAndDelete', async function (camp) {
+    if(camp){
+        await Review.deleteMany({
+            _id: { $in: camp.reviews }
+        })
+    }
+})
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
